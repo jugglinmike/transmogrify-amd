ef4837d57906d7c9a32603640b05a7d3c0bfb788
Tim Branyen <tim@tabdeveloper.com>
2014-01-17 22:13:42 -0500

    Run tests against real transformation lib.
    - Added in initial draft of AST transform lib.
    - Updated package.json to point to the above.
    - Corrected the malformed expected test for CJS deps.

--- test/fixtures/define/anon-function-cjs-deps/expected.js
+++ test/fixtures/define/anon-function-cjs-deps/expected.js
@@ -1,6 +1,9 @@
-(function() {
+var __AMDCLEAN0__ = (function() {
   var module = { exports: {} };
+
   (function(require, exports, module) {
     /* content */
   })(null, module.exports, module);
-});
+
+  return module.exports;
+})();
