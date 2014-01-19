8241c9407801f9c1e1dbcc86b26824d0b127a669
Mike Pennisi <mike@mikepennisi.com>
2014-01-18 19:58:55 -0500

    Improve test error reporting
    Include raw JavaScript source diffs on every failure by default.

--- readme.md
+++ readme.md
@@ -5,10 +5,3 @@
 Unit tests can be invoked from the command line via:
 
     $ npm test
-
-Failures will generate a graphical diff of the parsed ASTs. These failure
-reports may contain too much noise to be useful; in these cases, you can
-configure the test runner to report string diffs of the generated code (note
-that this may also contain more information than is relevant):
-
-    $ AMDCLEAN_STRINGS=1 npm test
--- test/util/assert.js
+++ test/util/assert.js
@@ -4,19 +4,23 @@ var assert = require("chai").assert;
 var bindAst = require("./bind-ast");
 
 assert.astMatch = function(actualSrc, expectedSrc) {
-  var actualAst = parse(actualSrc);
-  var expectedAst = parse(expectedSrc);
+  var actualAst = parse(actualSrc).body;
+  var expectedAst = parse(expectedSrc).body;
 
   bindAst(actualAst, expectedAst, /__AMDCLEAN\d+__/);
 
   try {
-    assert.deepEqual(actualAst, expectedAst);
-  } catch(err) {
-    if (process.env.AMDCLEAN_STRINGS) {
-      assert.equal(actualSrc, expectedSrc);
-    } else {
-      throw err;
-    }
+    assert.deepEqual(actualAst, expectedAst, "This is a message");
+  } catch(astError) {
+    // When ASTs do not match, compare ASTs with the original source so the
+    // generated error report contains diffs of both.
+    assert.deepEqual({
+      ast: actualAst,
+      source: actualSrc
+    }, {
+      ast: expectedAst,
+      source: expectedSrc
+    });
   }
 };
 
