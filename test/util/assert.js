9b1fe3ef1a416d1db20ce8e657971cc19b9848b4
Mike Pennisi <mike@mikepennisi.com>
2014-01-27 11:31:24 -0500

    Move AST comparison to separate library

--- test/util/assert.js
+++ test/util/assert.js
@@ -1,27 +1,11 @@
-var parse = require("esprima").parse;
+var compareAst = require("compare-ast");
 var assert = require("chai").assert;
 
-var bindAst = require("./bind-ast");
-
 assert.astMatch = function(actualSrc, expectedSrc) {
-  var actualAst = parse(actualSrc).body;
-  var expectedAst = parse(expectedSrc).body;
 
-  bindAst(actualAst, expectedAst, /__UNBOUND\d+__/);
-
-  try {
-    assert.deepEqual(actualAst, expectedAst, "This is a message");
-  } catch(astError) {
-    // When ASTs do not match, compare ASTs with the original source so the
-    // generated error report contains diffs of both.
-    assert.deepEqual({
-      ast: actualAst,
-      source: actualSrc
-    }, {
-      ast: expectedAst,
-      source: expectedSrc
-    });
-  }
+  compareAst(actualSrc, expectedSrc, {
+    varPattern: /__UNBOUND\d+__/
+  });
 };
 
 module.exports = assert;
