cb2b2e3f272553413253a83abf86b31c59d8fa42
Tim Branyen <tim@tabdeveloper.com>
2014-03-03 08:56:15 -0500

    Apply `module.exports` context to CJS definitions.
    Per CJS specification the context of a CJS module should refer to that
    module.

--- test/fixtures/define/function-mixed-cjs-deps/expected.js
+++ test/fixtures/define/function-mixed-cjs-deps/expected.js
@@ -2,7 +2,7 @@ __UNBOUND0__["__STRING1__"] = (function() {
   var __UNBOUND1__ = { exports: {} };
   var __UNBOUND2__ = (function(depA, exportsChangedForNoReason) {
     var content;
-  })(__UNBOUND0__["__STRING2__"], __UNBOUND1__.exports);
+  }).call(__UNBOUND1__.exports, __UNBOUND0__["__STRING2__"], __UNBOUND1__.exports);
 
   return __UNBOUND2__ || __UNBOUND1__.exports;
 })();
